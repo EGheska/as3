777c9c39aee26f5d01fca8f9ba5361fd72169c19
EGheska <dimanich30@gmail.com>
2023-03-23 15:42:38 +0100

    init

---
+++ rational.cpp
@@ -0,0 +1,57 @@
+//
+// Created by dima on 3/23/23.
+//
+
+#include "rational.h"
+
+rational::rational() {
+
+}
+
+rational::rational(int num) {
+
+}
+
+rational::rational(std::string str) {
+
+}
+
+int rational::num() const {
+    return 0;
+}
+
+int rational::den() const {
+    return 0;
+}
+
+std::string rational::str() const {
+    return nullptr;
+}
+
+bool rational::is_pos() const {
+    return false;
+}
+
+bool rational::is_neg() const {
+    return false;
+}
+
+bool rational::is_inf() const {
+    return false;
+}
+
+bool rational::is_nan() const {
+    return false;
+}
+
+void rational::num(int n) {
+
+}
+
+void rational::den(int d) {
+
+}
+
+void rational::str(std::string s) {
+
+}
---
+++ rational.h
@@ -0,0 +1,35 @@
+//
+// Created by dima on 3/23/23.
+//
+
+#ifndef AS3_RATIONAL_H
+#define AS3_RATIONAL_H
+
+#include <string>
+
+class rational {
+public:
+    /* Constructors */
+    rational();
+    rational(int num, int den);
+    rational(int num);
+    rational(std::string str);
+
+    /* Accessors */
+    int num() const;
+    int den() const;
+    std::string str() const;
+
+    bool is_pos() const;
+    bool is_neg() const;
+    bool is_inf() const;
+    bool is_nan() const;
+
+    /* Modifiers */
+    void num(int n);
+    void den(int d);
+    void str(std::string s);
+};
+
+
+#endif //AS3_RATIONAL_H
